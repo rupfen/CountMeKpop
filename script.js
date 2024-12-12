05f2961edcf70aa466e4224b4d6d76dd791a7a6c
rupfen <stefanie.haun@edu.pfefferwerk.de>
2024-12-12 13:26:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //Erfassen und Speichern der Lyrics aus dem ersten Eingabefeld.
-
-
-
-
+const 
+// 
+function werteAus() {
+    let  = LESEN.value;
+}
 
 //Erfassen des Suchbegriffs aus dem zweiten Eingabefeld.
